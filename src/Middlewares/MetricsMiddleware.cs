24a8fac3aab6fbeed92b3305cbda50104258139a
Alexander Lvovich <alex.lvovich@indevlabs.de>
2021-12-30 12:27:34 +0100

    using trace identificator

--- src/Middlewares/MetricsMiddleware.cs
+++ src/Middlewares/MetricsMiddleware.cs
@@ -1,7 +1,9 @@
 ﻿using Konso.Clients.Metrics.Models;
 using Konso.Clients.Metrics.Models.Requests;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Konso.Clients.Metrics.Middlewares
@@ -10,7 +12,8 @@ namespace Konso.Clients.Metrics.Middlewares
     {
         private readonly RequestDelegate _next;
         private readonly IMetricsServiceClient _metricsService;
-
+        private const string HeaderName = "X-Correlation-ID";
+        private static readonly string[] IgnoreMethods = { "OPTIONS" };
         public MetricsMiddleware(RequestDelegate next,
             IMetricsServiceClient metricsService)
         {
@@ -24,20 +27,27 @@ namespace Konso.Clients.Metrics.Middlewares
             // start timer
             var stopper = Stopwatch.StartNew();
 
+            // get correlation id from header 
+            if (context.Request.Headers.TryGetValue(HeaderName, out StringValues correlationId))
+            {
+                context.TraceIdentifier = correlationId;
+            }
+
             // invoke
             await _next(context);
 
             // stop
             stopper.Stop();
 
-            // no need to measure OPTIONS calls
-            if (context.Request.Method != "OPTIONS")
+            // no need to measure ignored calls
+            if (!IgnoreMethods.Contains(context.Request.Method))
             {
                 await _metricsService.CreateAsync(new CreateMetricsRequest()
                 {
                     Duration = stopper.ElapsedMilliseconds,
                     Name = $"{context.Request.Method} {context.Request.Path.ToString()}",
-                    ResponseCode = context.Response != null ? context.Response.StatusCode : new int?()
+                    ResponseCode = context.Response != null ? context.Response.StatusCode : new int?(),
+                    CorrelationId = context.TraceIdentifier
                 });
             }
                 
